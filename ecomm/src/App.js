836f926436e2465c018d6fe29dd357bea5a1bbd3
RittamDas <Rittamdas98@hotmail.co.uk>
2020-11-25 23:03:57 +0530

    routing the page

--- ecomm/src/App.js
+++ ecomm/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <h1>AMAZON SHIT</h1>
+    <Router>
+         <div className="App">
+           <Switch>
+             <Route path = "/">
+           <h1>AMAZON SHIT</h1>
+           </Route>
+             <Route path = "/login">
+               <h1>Login Page</h1>
+             </Route>
+           </Switch>
+              
          </div>
+    </Router>
+    
   );
 }
 
