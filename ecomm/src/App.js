8bc7d279bc7f3edf2f34074a3280c95783a9216b
RittamDas <Rittamdas98@hotmail.co.uk>
2020-12-13 18:16:34 +0530

    finished building banner

--- ecomm/src/App.js
+++ ecomm/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import Home from './Home';
 function App() {
   return (
     <Router>
@@ -9,6 +10,7 @@ function App() {
            <Switch>
              <Route exact path = "/">
                <Header />
+               <Home />
            <h1>AMAZON SHIT</h1>
            </Route>
              <Route exact path = "/login">
